0accfe25639e2c229e93e1d184f318693d744157
rafihaque7 <rafihaque7@gmail.com>
2017-07-25 16:11:16 -0700

    more added

--- src/Peach.java
+++ src/Peach.java
@@ -7,7 +7,8 @@ import java.net.URL;
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.math.*;
-
+import java.nio.Buffer;
+import java.awt.Graphics;
 /**
  * Created by rafihaque on 6/24/17.
  */
@@ -16,12 +17,14 @@ public class Peach extends JPanel {
     private BufferedImage dayImg;
     private BufferedImage nightImg;
     private BufferedImage navyImg;
+    private BufferedImage curveImg;
 
     public Peach(){
         try {
             dayImg = ImageIO.read(new File("src/day.jpg"));
             nightImg = ImageIO.read(new File("src/night.jpg"));
             //navyImg = ImageIO.read(new URL("http://api.usno.navy.mil/imagery/earth.png?date=6/17/2012"));
+            curveImg  = ImageIO.read(new File("src/origcurve.png"));
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -34,42 +37,49 @@ public class Peach extends JPanel {
     }
 
     //J is the day of the year, L is latitude
-    static double getY(int J, int L)
+
+    //Calculates the declination δ of the sun(All angles measured in degrees)
+    public static double declination(double d)
     {
-        double P =  Math.asin(.39795*Math.cos(.2163108 + 2*Math.atan(.9671396*Math.tan(.00860*(J-186)))));
-        return 24 - (24/Math.PI)*Math.acos( (Math.sin(0.8333*Math.PI/180) + Math.sin(L*Math.PI/180)*Math.sin(P)) / (Math.cos(L*Math.PI/180)*Math.cos(P)) );
+        double M = -3.6 + 0.9856d;
+        double v = M + 1.9*(Math.sin(M));
+        double λ = v + 102.9;
+        return 22.8*Math.sin(λ) + 0.6*Math.pow(Math.sin(λ),3);
     }
 
-    static double pixeltoLat(double pix)
+    public static double l(double t)
     {
-        return pix/7.777777777778 - 90;
+        return -15 * t;
     }
 
-    static double longtoPix(double longt)
-    {
-        return (longt+180)*1.944444444444444;
-    }
-    public void paintComponent(Graphics g){
+
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
         //this.setBackground(Color.WHITE);
-        //g.drawImage(dayImg,0,0,1400,700,this);
+        g.drawImage(dayImg, 0, 0, 1400, 700, this);
 
+
+        //g.drawImage(curveImg,0,0,1400,700,this);
+        g.setColor(Color.black);
+        //drawPoint(g,100,100);
+
+        for(int i=0; i<=360; i++)
+        {
+            double x = (-1*Math.cos(l(12))*Math.sin(declination(206))*Math.sin(i)-Math.sin(l(12))*Math.cos(i));
+            double y = (-1*Math.sin(l(12))*Math.sin(declination(206))*Math.sin(i)+Math.cos(l(12))*Math.cos(i));
+            drawPoint(g,(int)Math.asin(Math.cos(declination(206))*Math.sin(i)),(int)Math.atan2(y,x));
+        }
         //g.drawImage(navyImg,0,0,1400,700,this);
-        g.setColor(Color.BLACK);
 
-        //g.drawLine(0,0,200,200);
-        //g.setColor(Color.black);
-        //g.fillRect(100,100,300,300);
-        //g.drawOval(250,250,30,30);
 
         //drawPoint(g,250,250);
 
         int dayoftheyear = 5;
-        for(int lat=-0; lat<1400; lat++)
-        {
-            drawPoint(g,lat,(int)longtoPix((int)getY(dayoftheyear,(int)pixeltoLat(lat))));
-        }
+
 
 
     }
 }
+
+//    drawPoint(g,(int)(-1*Math.cos(l(12))*Math.sin(declination(206))*Math.sin(i)-Math.sin(l(12))*Math.cos(i)),(int)(-1*Math.sin(l(12))*Math.sin(declination(206))*Math.sin(i)+Math.cos(l(12))*Math.cos(i)));
